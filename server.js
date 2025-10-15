69e88e039522042e578c91bb143378d80c695a3b
Murad Orucov <78502364+muradorucov@users.noreply.github.com>
2025-10-15 12:54:06 +0400

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const router = require('./src/routers');
 const dotenv = require("dotenv");
 dotenv.config();
 
-var whitelist = [
-    process.env.GLOBAL_ORIGIN,
-    "http://127.0.0.1:5500/"
-    // process.env.LOCAL_ORIGIN
-]
+// var whitelist = [
+//     process.env.GLOBAL_ORIGIN,
+//     "http://127.0.0.1:5500/"
+//     // process.env.LOCAL_ORIGIN
+// ]
 
-var corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
+// var corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin) return callback(null, true);
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 
